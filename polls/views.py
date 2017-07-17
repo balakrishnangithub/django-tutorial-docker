4142c80fb583828bf46051608f4eb82fcef4278e
Balakrishnan <light.bala@gmail.com>
2017-07-17 12:54:51 +0530

    remove `race conditions`

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.db.models import F
 
 from .models import Choice, Question
 
@@ -34,7 +35,7 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
