5ea9faf8b2c0c46f3697e08d21f21a15bde8f771
Balakrishnan <light.bala@gmail.com>
2017-07-18 17:09:50 +0530

    update views to generic views

--- polls/urls.py
+++ polls/urls.py
@@ -5,12 +5,20 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    # ex: /polls/
-    url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
+    url(
+        r'^$',
+        views.IndexView.as_view(),  # IndexView inherits `generic.ListView`
+        name='index'
+    ),
+    url(
+        r'^(?P<pk>[0-9]+)/$',  # `generic.DetailView` expects the primary key value
+        views.DetailView.as_view(),  # DetailView inherits `generic.DetailView`
+        name='detail'
+    ),
+    url(
+        r'^(?P<pk>[0-9]+)/results/$',
+        views.ResultsView.as_view(),
+        name='results'
+    ),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
