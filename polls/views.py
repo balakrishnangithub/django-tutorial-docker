999a31ad077a28198039648f5039dd45bcb8bea2
Balakrishnan <light.bala@gmail.com>
2017-08-08 18:21:09 +0530

    fix listing or displaying future/unpublished questions

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -12,15 +13,24 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Don't list future(unpublished) questions
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
     # Django is able to determine an appropriate name for the context variable from `model`
     # eg. in this case context_object_name = 'question'
+
+    def get_queryset(self):
+        """
+        Don't display future(unpublished) question
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
