54c02026658e2a7185edee6807a2fbca65949521
Balakrishnan <light.bala@gmail.com>
2017-08-11 15:32:30 +0530

    move Choice to QuestionAdmin

--- polls/admin.py
+++ polls/admin.py
@@ -3,15 +3,20 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(
     Question,  # model
     QuestionAdmin  # admin class
 )
-admin.site.register(Choice)
