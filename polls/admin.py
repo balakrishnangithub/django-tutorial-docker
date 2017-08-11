d76b54733868f0dbad59c997c3edb54d80723017
Balakrishnan <light.bala@gmail.com>
2017-08-11 14:06:53 +0530

    update fieldsets in Django Admin Question

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,10 @@ from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']  # reordering the fields
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 
 admin.site.register(
